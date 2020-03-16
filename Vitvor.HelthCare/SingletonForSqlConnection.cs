2404427493a3918675d62f1adddc8bc8eedbd993
lesha25012001 <lesha25012001@outlook.com>
2020-03-16 14:36:07 +0300

    Добавьте файлы проекта.

---
+++ Vitvor.HelthCare/SingletonForSqlConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitvor.HelthCare
+{
+    class SingletonForSqlConnection
+    {
+        private static SingletonForSqlConnection instance;
+        public static SqlConnection SqlConnection { get; private set; }
+        private SingletonForSqlConnection()
+        {
+            SqlConnection = new SqlConnection("Data Source=ALEKS;Initial Catalog=CourseProject;Integrated Security=True");
+            SqlConnection.Open();
+        }
+        public static SingletonForSqlConnection getInstance()
+        {
+            if (instance == null)
+                instance = new SingletonForSqlConnection();
+            return instance;
+        }
+        public static void Close()
+        {
+            if (SqlConnection.State == System.Data.ConnectionState.Open)
+                SqlConnection.Close();
+        }
+    }
+}

910a8b2d9b55db8f0b98b9477932c4d7f4294fc4
lesha25012001 <lesha25012001@outlook.com>
2020-04-02 21:18:02 +0300

    add part of cryptography

---
+++ Vitvor.HelthCare/EmailPassword/Password.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Vitvor.HelthCare
+{
+    class Password
+    {
+        public SecureString PasswordString { get; private set; }
+        public byte[] Key { get; private set; }
+        public byte[] IV { get; private set; }
+        public byte[] PasswordBytes { get; private set; }
+        public Password()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(byte[]));
+            using(FileStream fs=new FileStream("Key.xml", FileMode.Open))
+            {
+                Key = (byte[])serializer.Deserialize(fs);
+            }
+            using(FileStream fs=new FileStream("IV.xml", FileMode.Open))
+            {
+                IV = (byte[])serializer.Deserialize(fs);
+            }
+            using(FileStream fs=new FileStream("Pass.xml", FileMode.Open))
+            {
+                PasswordBytes = (byte[])serializer.Deserialize(fs);
+            }
+        }
+    }
+}
